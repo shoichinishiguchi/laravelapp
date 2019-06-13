734eb9619e146a7da57c1ab304d4b2c0807fe94d
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2019-06-14 00:15:10 +0900

    Commit

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -9,33 +9,15 @@ use Illuminate\Http\Response;
 class HelloController extends Controller
 {
 
-    public function index(Request $request, Response $response){
-
-$html = <<<EOF
-<html>
-<head>
-<title>Hello/Index</title>
-<style>
-body {font-size: 16pt; color: #999; }
-h1 { font-size: 120pt; text-align:right; color: #fafafa; margin: -50px 0px -120px 0px;}
-</style>
-</head>
-<body>
-    <h1>Hello</h1>
-    <h3>Request</h3>
-    <pre>{$request}</pre>
-    <h3>Response</h3>
-    <pre>{$response}</pre>
-</body>
-</html>
-EOF;
-        $response->setContent($html);
-        $request->path();
-        return $response;
-
+    public function index(Request $request)
+    {
+        $data =
+        [
+            'msg' => 'これはBladeを利用したサンプルです。'
+        ];
+        return view('hello.index', $data);
     }
-}
-
+};
 
 // public function index() {
 //     global $head, $style, $body, $end;
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Route::get('hello', function(){
+//     return view('hello.index');
+// });
+
+
 Route::get('hello', 'HelloController@index');
 // Route::get('hello/other', 'HelloController@other');
 
