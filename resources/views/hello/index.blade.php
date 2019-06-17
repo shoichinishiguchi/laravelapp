e080c8d6a72bea2d846271355134978542853022
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2019-06-18 08:17:51 +0900

    Commit

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -1,4 +1,31 @@
-<html>
+@extends('layouts.helloapp')
+@section('title', 'Index')
+@section('menubar')
+  @parent
+  インデックスページ
+@endsection
+
+@section('content')
+  <p>ここが本文のコンテンツです。</p>
+  <p>必要なだけ記述できます。</p>
+
+  @component('components.message')
+    @slot('msg_title')
+      CAUTION!
+    @endslot
+
+    @slot('msg_content')
+      これはメッセージの表示です。
+    @endslot
+  @endcomponent
+
+@endsection
+
+@section('footer')
+copyright 2017 tuyano.
+@endsection
+
+<!-- <html>
 <head>
   <title>Hello/Index</title>
   <style>
@@ -22,7 +49,7 @@
     @endwhile
     </ol>
 </body>
-</html>
+</html> -->
 
 <!-- <h1>Blade/Index</h1>
     <p>$#064;forディレクティブの例</p>
