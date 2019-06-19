9a4db7393743100634abf643c0b3e08a2d5c67b5
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2019-06-19 11:47:04 +0900

    commit

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -7,9 +7,8 @@
 
 @section('content')
   <p>ここが本文のコンテンツです。</p>
-
-  @each('components.item', $data, 'item')
-
+  <p>Controller value<br>'message' = {{$view_message}}</p>
+  <p>ViewComposer value<br>'view_message' = {{$view_message}}</p>
 @endsection
 
 @section('footer')
